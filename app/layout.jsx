e236a01dda21a819f082a6680f1d4b119fea4b93
tunahan <ttezcan.1999@gmail.com>
2023-07-29 21:58:41 +0300

    init proje

---
+++ app/layout.jsx
@@ -0,0 +1,20 @@
+import "@/styles/global.scss";
+import { Ubuntu } from "next/font/google";
+
+export const metadata = {
+  title: "Layout",
+  description: "This is the layout",
+};
+const ubuntu = Ubuntu({
+  weight: "500",
+  subsets: ["latin"],
+});
+export default function layout({ children }) {
+  return (
+    <html lang="en" className={ubuntu.className}>
+      <body>
+        <main>{children}</main>
+      </body>
+    </html>
+  );
+}
